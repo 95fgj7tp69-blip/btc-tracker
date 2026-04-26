c215219f9d884a80eba03a26e7fd55e5b942bf6f
95fgj7tp69-blip <95fgj7tp69@privaterelay.appleid.com>
2026-04-26 15:59:25 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// Kommentar
 import { useState, useEffect, useCallback, useRef } from "react";
 import { Area, AreaChart, ResponsiveContainer, YAxis, XAxis, Tooltip } from "recharts";
 import { createClient } from "@supabase/supabase-js";
